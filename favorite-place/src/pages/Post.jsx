04e0d9d0e5b54aa3be38b0bbfa357ab28345cb91
evymari <144184061+evymari@users.noreply.github.com>
2024-09-10 13:26:24 +0200

    none

---
+++ favorite-place/src/pages/Post.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CreatePost from '@/components/socialNetworks/CreatePost';
+
+
+
+const Post = () => {
+  return (
+ <CreatePost></CreatePost>
+  )
+}
+
+export default Post
--- favorite-place/src/pages/pages3.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const pages3 = () => {
-  return (
-    <div>pages3</div>
-  )
-}
-
-export default pages3
