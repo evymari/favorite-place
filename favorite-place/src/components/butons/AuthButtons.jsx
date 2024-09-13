4c7263ae66495ee2384cff56936df6e0f83b6445
evymari <144184061+evymari@users.noreply.github.com>
2024-09-13 10:00:46 +0200

    [FO-1][FP-15]feat - user profile display

--- favorite-place/src/components/butons/AuthButtons.jsx
+++ favorite-place/src/components/butons/AuthButtons.jsx
@@ -1,35 +1,33 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 export default function AuthButtons() {
-  
+
   const [isLogin, setIsLogin] = useState(true);
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   return (
     <div className="flex justify-center mt-10">
       <div className="bg-purple-200 p-1 rounded-full flex">
-        
+
         <button
           onClick={() => {
             setIsLogin(true);
-            navigate('/login'); 
+            navigate('/login');
           }}
-          className={`w-32 py-2 rounded-full ${
-            isLogin ? 'bg-gray-400 text-white' : 'bg-purple-200 text-gray-600'
-          } transition-colors duration-300`}
+          className={`w-32 py-2 rounded-full ${isLogin ? 'bg-gray-400 text-white' : 'bg-purple-200 text-gray-600'
+            } transition-colors duration-300`}
         >
           Log In
         </button>
 
-        
+
         <button
           onClick={() => {
             setIsLogin(false);
-            navigate('/signup'); 
+            navigate('/signup');
           }}
-          className={`w-32 py-2 rounded-full ${
-            !isLogin ? 'bg-gray-400 text-white' : 'bg-purple-200 text-gray-600'
-          } transition-colors duration-300`}
+          className={`w-32 py-2 rounded-full ${!isLogin ? 'bg-gray-400 text-white' : 'bg-purple-200 text-gray-600'
+            } transition-colors duration-300`}
         >
           Sign Up
         </button>
